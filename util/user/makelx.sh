d8f2537c3948953a1bdabab58e35349302978e81
mesheets <16882600+mesheets@users.noreply.github.com>
2024-07-13 17:12:05 -0400

    Update makelx.sh

--- util/user/makelx.sh
+++ util/user/makelx.sh
@@ -18,8 +18,12 @@ shift
 ADDITIONAL_SOURCES=$*
 
 # Usage information
-echo "Makes an LX program file from source"
+echo "Generate an executable LX program file from source code"
 echo "Usage: $SCRIPT_NAME <main source file> [additional source files]..."
+echo ""
+echo "Usage Notes:"
+echo " * File names cannot contain spaces, due to \"make\" command limitations."
+echo "   - https://savannah.gnu.org/bugs/?712"
 
 echo ""
 echo "Main program source file: '$MAIN_PROGRAM_SOURCE'"
@@ -30,9 +34,5 @@ echo " - $ADDITIONAL_SOURCES"
 MAKE_COMMAND="${MAKE_PROGRAM} SOURCES='${MAIN_PROGRAM_SOURCE}' ${PROGRAM_NAME}_SRC='${ADDITIONAL_SOURCES}'"
 
 echo ""
-echo "Command to execute: ${MAKE_COMMAND}"
+echo "Command: \"${MAKE_COMMAND}\""
 echo "(Placeholder only - functionality not yet implemented)"
-
-echo ""
-echo "NOTE: make cannot handle spaces in file names"
-echo " - https://savannah.gnu.org/bugs/?712"
