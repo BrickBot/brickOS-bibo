075ef15109fff278b8ed10287aad6c898347218f
mesheets <16882600+mesheets@users.noreply.github.com>
2024-07-13 13:46:41 -0400

    Create the structure for adding user programs

---
+++ util/user/makelx.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+### ==========================================================================
+###  $Id: makelx.sh  |  Matthew Sheets
+###  FILE: util/user/makelx.sh - script for building user programs
+###  MAKE: will both build and install as "makelx" with 755 permissions
+###  bibo - another LEGO Mindstorms OS
+### --------------------------------------------------------------------------
+
+echo Makes an LX program file from source
+echo Usage: $0 <main source file> [additional source files]...
+
+echo ""
+echo (Placeholder only - functionality not yet implemented)
