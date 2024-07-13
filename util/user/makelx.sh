d3428ec59328ec0275e947532f59bfff32accd64
mesheets <16882600+mesheets@users.noreply.github.com>
2024-07-13 16:31:31 -0400

    Setup basic variables for makelx.sh

--- util/user/makelx.sh
+++ util/user/makelx.sh
@@ -7,15 +7,32 @@
 ###  bibo - another LEGO Mindstorms OS
 ### --------------------------------------------------------------------------
 
+# Constants
+MAKE_PROGRAM=make
+
+# Variable Initialization
+SCRIPT_NAME=$(basename $0)
+MAIN_PROGRAM_SOURCE=$1
+PROGRAM_NAME=$(basename ${1%.*})
+shift
+ADDITIONAL_SOURCES=$*
+
+# Usage information
 echo "Makes an LX program file from source"
-echo "Usage: $(basename $0) <main source file> [additional source files]..."
+echo "Usage: $SCRIPT_NAME <main source file> [additional source files]..."
 
 echo ""
+echo "Main program source file: '$MAIN_PROGRAM_SOURCE'"
+
+echo "Additional source files for $PROGRAM_NAME:"
+echo " - $ADDITIONAL_SOURCES"
+
+MAKE_COMMAND="${MAKE_PROGRAM} SOURCES='${MAIN_PROGRAM_SOURCE}' ${PROGRAM_NAME}_SRC='${ADDITIONAL_SOURCES}'"
+
+echo ""
+echo "Command to execute: ${MAKE_COMMAND}"
 echo "(Placeholder only - functionality not yet implemented)"
 
 echo ""
-echo "Main source file: '$1'"
-echo "Additional source files for $(basename ${1%.*}):"
-shift
-echo " - $*"
-
+echo "NOTE: make cannot handle spaces in file names"
+echo " - https://savannah.gnu.org/bugs/?712"
