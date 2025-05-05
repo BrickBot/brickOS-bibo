163adb7490233921ade550abe127d938e2a02fdf
mesheets <16882600+mesheets@users.noreply.github.com>
2025-05-05 17:50:35 -0400

    Address compiler warning in conio.h

--- include/conio.h
+++ include/conio.h
@@ -67,14 +67,14 @@ extern "C" {
 //! Table lookup: list of native patterns, one for each HEX character
 /*! \param nibble HEX char value (0-9, a-f)
 */
-extern inline char get_hex_display_code(unsigned char nibble);
+extern char get_hex_display_code(unsigned char nibble);
 
 #ifdef CONF_ASCII
 
 //! Table lookup: list of native patterns, one for each ASCII character
 /*! \param c ASCII char value (least significant 7 bits ONLY)
 */
-extern inline char get_ascii_display_code(unsigned char c);
+extern char get_ascii_display_code(unsigned char c);
 
 #endif // CONF_ASCII
 
